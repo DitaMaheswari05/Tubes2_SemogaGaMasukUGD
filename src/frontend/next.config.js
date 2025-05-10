29c384e61bc14149a90b836914ab16cb3a517a5d
DitaMaheswari05 <ditamaheswari05@gmail.com>
2025-05-10 15:36:13 +0700

    feat : update config

--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -1,11 +1,25 @@
-// next.config.js
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://recipe-backend:8080/api/:path*',
+        source: '/api/elements',
+        destination: 'http://localhost:8080/api/elements'
       },
-    ]
-  },
-}
+      {
+        source: '/api/find',
+        destination: 'http://localhost:8080/api/find'
+      },
+      {
+        source: '/api/recipes',
+        destination: 'http://localhost:8080/api/recipes'
+      },
+      {
+        source: '/api/svgs/:path*',
+        destination: 'http://localhost:8080/svgs/:path*'
+      }
+    ];
+  }
+};
+
+module.exports = nextConfig;
